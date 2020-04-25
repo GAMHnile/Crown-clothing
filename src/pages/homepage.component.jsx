1662fbb26dd83c17a302356cb3d5004b9b08efd0
GAMHnile <georgeamhuweh@gmail.com>
2020-04-25 10:36:10 +0100

    Adding hover effect to menu-items componeent

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.scss';
+import DirectoryMenu from '../components/directory/directory.component';
+
+
+const HomePage =()=>{
+    return(
+        <div className='homepage'>
+            <DirectoryMenu /> 
+        </div>
+
+    )
+}
+
+
+export default HomePage;
