af7f2a71a3a8974da28d687ca4650cfa06827283
GAMHnile <georgeamhuweh@gmail.com>
2020-04-25 19:16:51 +0100

    created shop page preview, changed padding on app.scss

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
 import {Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
-const Hats=()=> (
-  <h1>Hats Page</h1>
-)
+
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={Hats} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
