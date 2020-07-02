bfe1aebb9c20c7d649a58253025dd8ce5a18b52d
George Uweh <59946617+GAMHnile@users.noreply.github.com>
2020-07-02 21:44:57 +0100

    Optimized code with react lazy and suspence. Created spinner component to display while lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, lazy, Suspense} from 'react';
 
 import {GlobalStyle} from './global.syles';
 
 import Header from './components/header/header.component.jsx';
-
-import HomePage from './pages/homepage.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
 
 import {Route, Switch, Redirect, useLocation} from 'react-router-dom';
 
@@ -17,6 +12,12 @@ import {checkUserSession} from './redux/user/user.actions';
 import {createStructuredSelector} from 'reselect';
 import {selectCurrentUser} from './redux/user/user.selectors';
 
+import Spinner from './components/spinner/spinner.component';
+
+const HomePage = lazy(()=>import('./pages/homepage.component'));
+const SignInAndSignUpPage = lazy(()=>import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'));
+const ShopPage = lazy(()=>import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(()=>import('./pages/checkout/checkout.component'));
 
 
 const App=({checkUserSession, currentUser})=> {
@@ -37,14 +38,16 @@ const App=({checkUserSession, currentUser})=> {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout' component={CheckoutPage} />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
 
-        <Route 
-        exact 
-        path='/signin' 
-        render={()=>currentUser? <Redirect to='/' /> : <SignInAndSignUpPage />} />
+          <Route 
+          exact 
+          path='/signin' 
+          render={()=>currentUser? <Redirect to='/' /> : <SignInAndSignUpPage />} />
+        </Suspense>
       </Switch>
     </div>
   );
