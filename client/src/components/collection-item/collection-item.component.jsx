d1908d875bcccf9129ea685c7e5d211e3260b958
George Uweh <59946617+GAMHnile@users.noreply.github.com>
2020-07-06 17:24:55 +0100

    added saveCart to collection-item

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
-import {connect} from 'react-redux';
-import {addItem} from '../../redux/cart/cart.actions'
 
-const CollectionItem=( {item,addItem})=>{
+import {connect} from 'react-redux';
+
+import {createStructuredSelector} from 'reselect';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+import {selectCurrentUser} from '../../redux/user/user.selectors';
+
+import {addItem} from '../../redux/cart/cart.actions';
+import {saveUserCart} from'../../redux/cart/cart.actions';
+
+const CollectionItem=( {item,addItem, saveCart, currentUser, allCartItems})=>{
     const {name, imageUrl, price}=item;
     return(
     <div className='collection-item'>
@@ -13,13 +20,24 @@ const CollectionItem=( {item,addItem})=>{
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <CustomButton className='custom-button' onClick={()=>addItem(item)} inverted>Add to cart</CustomButton>
+        <CustomButton className='custom-button' 
+        onClick={()=>{
+            addItem(item);
+            saveCart({userId: currentUser.id, userCart: allCartItems});
+        }} 
+        inverted>Add to cart</CustomButton>
     
     </div>
 )}
 
-const mapDispatchToProps=(dispatch)=>({
-    addItem: item=>dispatch(addItem(item))
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    allCartItems: selectCartItems
 })
 
-export default connect(null,mapDispatchToProps)(CollectionItem);
+const mapDispatchToProps=(dispatch)=>({
+    addItem: item=>dispatch(addItem(item)),
+    saveCart: (userIdAndCart)=>dispatch(saveUserCart(userIdAndCart))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
